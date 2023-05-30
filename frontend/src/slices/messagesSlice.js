88d828ed7e61f50c2022348030d5cb2280c22b58
ramil290989 <garifullinramil290989@gmail.com>
2023-05-31 03:26:21 +0500

    step 5

--- frontend/src/slices/messagesSlice.js
+++ frontend/src/slices/messagesSlice.js
@@ -1,18 +1,28 @@
 import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import _ from 'lodash';
+import { fetchChannels } from './channelsSlice.js';
 
 const messagesAdapter = createEntityAdapter();
 
-const initialState = messagesAdapter.getInitialState();
+const initialState = messagesAdapter.getInitialState({
+  currentChannel: {},
+});
 
 const messagesSlice = createSlice({
   name: 'messages',
   initialState,
   reducers: {
     addMessage: messagesAdapter.addOne,
-    addMessages: messagesAdapter.addMany,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchChannels.fulfilled, (state, action) => {
+        messagesAdapter.addMany(state, action.payload.messages);
+        state.currentChannel = _.find(action.payload.channels, function(channel) { return channel.id === action.payload.currentChannelId});
+      })
   },
 });
 
-export const selectors = messagesAdapter.getSelectors((state) => state.messagesReducer);
+export const selectors = messagesAdapter.getSelectors((state) => state.messages);
 export const { actions } = messagesSlice;
 export default messagesSlice.reducer;
