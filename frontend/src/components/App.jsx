c05d4fdf169ea1539672c8a1aae45e3e1232bc0e
ramil290989 <garifullinramil290989@gmail.com>
2023-06-22 02:02:36 +0500

    step 11-6

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -11,11 +11,15 @@ import PageNotFound from './PageNotFound.jsx';
 import MainPage from './MainPage.jsx'
 import LogIn from './LogIn.jsx';
 import SignUp from './SignUp.jsx';
-import { Provider, ErrorBoundary } from '@rollbar/react';
+import { Provider as RollbarProvider, ErrorBoundary, LEVEL_WARN } from '@rollbar/react';
 
 const rollbarConfig = {
   accessToken: 'fe5326e87ba64a50a587c0b31fd4f69c',
-  environment: 'testenv',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
 };
 
 function TestError() {
@@ -29,22 +33,21 @@ const App = () => {
   
   return (
     <Provider config={rollbarConfig}>
-      <ErrorBoundary>
-        <TestError />
-    <AuthorizationData.Provider value={[authorizationData, setAuthorizationData]}>
-      <div className="d-flex flex-column h-100">
-        <HeaderNav />
-        <BrowserRouter>
-          <Routes>
-            <Route path="*" element={<PageNotFound />} />
-            <Route path="/" element={authorizationData.token ? <MainPage /> : <LogIn />} />
-            <Route path="login" element={<LogIn />} />
-            <Route path="signup" element={<SignUp />} />
-          </Routes>
-        </BrowserRouter>
-        <ToastContainer autoClose={2000} />
-      </div>
-    </AuthorizationData.Provider>
+      <ErrorBoundary level={LEVEL_WARN} fallbackUI={ErrorDisplay}>
+        <AuthorizationData.Provider value={[authorizationData, setAuthorizationData]}>
+          <div className="d-flex flex-column h-100">
+            <HeaderNav />
+            <BrowserRouter>
+              <Routes>
+                <Route path="*" element={<PageNotFound />} />
+                <Route path="/" element={authorizationData.token ? <MainPage /> : <LogIn />} />
+                <Route path="login" element={<LogIn />} />
+                <Route path="signup" element={<SignUp />} />
+              </Routes>
+            </BrowserRouter>
+            <ToastContainer autoClose={2000} />
+          </div>
+        </AuthorizationData.Provider>
     </ErrorBoundary>
     </Provider>
   );
